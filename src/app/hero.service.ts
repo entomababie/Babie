c40e15d30f421a7ddf0647c7a7f0472a62820d6d
GEC <gec@GECs-iMac.local>
2018-01-10 10:14:49 +0800

    Add spaces

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -75,11 +75,18 @@ private handleError<T> (operation = 'operation', result?: T) {
     console.error(error); // log to console instead
  
     // TODO: better job of transforming error for user consumption
-    this.log(`${operation} failed: ${error.message}`);
+    this.log(`${operation} failed: ${error.message}`);    
  
     // Let the app keep running by returning an empty result.
     return of(result as T);
   };
+}
+
+updateHero (hero: Hero): Observable<any> {
+  return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+    tap(_ => this.log(`updated hero id=${hero.id}`)),
+    catchError(this.handleError<any>('updateHero'))
+  );
 }
   
 
